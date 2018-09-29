00e053354b60c9227ff05c47a855e4d56eb8844f
klacol <klaus.aengenvoort@etask.de>
2018-09-29 10:36:48 +0200

    Build the Tools solution #21 Test with AppVeyor

--- ValidateSampleFiles.ps1
+++ ValidateSampleFiles.ps1
@@ -1,9 +1,13 @@
-﻿$ValidatorExe="C:\projects\productdata\Tools\SchemaValidator\bin\Release\SchemaValidator.exe"
-$SampleFiles = Get-ChildItem -Path "C:\projects\productdata"  -Recurse -Include *.ifc,*.ifcXML
+﻿$BaseFolder = "C:\projects\productdata"
+$ValidatorExe="$BaseFolder\Tools\SchemaValidator\bin\Release\SchemaValidator.exe"
+$SampleFiles = Get-ChildItem -Path $BaseFolder  -Recurse -Include *.ifc,*.ifcXML
+$ExitCode=0
 foreach ($file in $SampleFiles | Sort-Object -Property FullName) 
 {
    Write-Host "++++++++++++++++++++++++++++++++++++++"
    Write-Host Validating $file.Name
    Write-Host "++++++++++++++++++++++++++++++++++++++"  
    & $ValidatorExe $file.FullName
+   if ($LastExitCode -eq '1') {$ExitCode=1}
 }
+Exit $ExitCode
