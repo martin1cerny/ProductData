40c9cf8135b62bcb288e629d182597b795b33547
klacol <klaus.aengenvoort@etask.de>
2018-09-29 10:04:49 +0200

    Build the Tools solution #21 Test with AppVeyor

---
+++ ValidateSampleFiles.ps1
@@ -0,0 +1,9 @@
+﻿$ValidatorExe="Tools\SchemaValidator\bin\Release\SchemaValidator.exe"
+$SampleFiles = Get-ChildItem  -Recurse -Include *.ifc,*.ifcXML
+foreach ($file in $SampleFiles | Sort-Object -Property FullName) 
+{
+   Write-Host "++++++++++++++++++++++++++++++++++++++"
+   Write-Host Validating $file.Name
+   Write-Host "++++++++++++++++++++++++++++++++++++++"  
+   & $ValidatorExe $file.FullName
+}
