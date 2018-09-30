2b268ece25df027492a9958d9718e47b6a13386f
klacol <klaus.aengenvoort@etask.de>
2018-09-30 22:54:59 +0200

    Build the Tools solution #21 Test with AppVeyor

--- ValidateSampleFiles.ps1
+++ ValidateSampleFiles.ps1
@@ -6,11 +6,15 @@ foreach ($file in $SampleFiles | Sort-Object -Property FullName)
 {
    Write-Host "++++++++++++++++++++++++++++++++++++++"
    Write-Host Validating $file.Name
-   Write-Host "++++++++++++++++++++++++++++++++++++++"  
    & $ValidatorExe $file.FullName
-   if ($LastExitCode -eq '1') {$ExitCode=1}
+   if ($LastExitCode -eq '0') 
+   { 
+     Write-Host "OK - $file.Name is valid" -ForegroundColor Green
+   }
+   else 
+   {
+     Write-Host "ERROR - $file.Name is not valid" -ForegroundColor Red
+     $ExitCode=1
+   }
 }
-if ($ExitCode -eq '0') 
-    { Write-Host "OK - The file is valid" -ForegroundColor Green}
-else {Write-Host "ERROR - The file is not valid" -ForegroundColor Red}
 Exit $ExitCode
