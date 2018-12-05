2b695f62335b961e89bf06b37ad5f96f2ee68b27
Martin Cerny <martin.cerny@xbim.cz>
2018-12-05 08:13:33 +0100

    Updated to new csproj format and latest xBIM libraries. Added support for IFD modelling.

--- Tools/SchemaValidator/Logger.cs
+++ Tools/SchemaValidator/Logger.cs
@@ -1,92 +1,104 @@
-﻿using log4net;
-using log4net.Appender;
-using log4net.Core;
-using log4net.Layout;
-using log4net.Repository.Hierarchy;
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xbim.Common;
 
 namespace SchemaValidator
 {
     public class Logger
     {
         // https://stackoverflow.com/a/19538654/6345585
-        public static ErrorAppender Setup(string file = null)
+        public static MemoryLog Setup(string file = null)
         {
-            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
-            // clear in case this is not the first run
-            hierarchy.ResetConfiguration();
+            var config = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.ColoredConsole();
 
-            PatternLayout patternLayout = new PatternLayout
-            {
-                // ConversionPattern = "%date [%thread] %-5level %logger - %message%newline"
-                ConversionPattern = "%date %-5level - %message%newline"
-            };
-            patternLayout.ActivateOptions();
 
             if (file != null)
             {
-                RollingFileAppender roller = new RollingFileAppender
-                {
-                    AppendToFile = false,
-                    File = file,
-                    Layout = patternLayout,
-                    MaxSizeRollBackups = 5,
-                    MaximumFileSize = "1GB",
-                    RollingStyle = RollingFileAppender.RollingMode.Size,
-                    StaticLogFileName = true,
-                    Threshold = Level.Warn
-                };
-                roller.ActivateOptions();
-                hierarchy.Root.AddAppender(roller);
+                config.WriteTo.File(file);
             }
 
-            var appender = new ErrorAppender();
-            appender.ActivateOptions();
-            hierarchy.Root.AddAppender(appender);
+            var memLog = new MemoryLog();
+            var memLogProvider = new MemoryLogProvider(memLog);
+            
+            Log.Logger = config.CreateLogger();
 
-            // add console appender to see what is happenning
-            var csl = new ColoredConsoleAppender
-            {
-                Layout = patternLayout,
-                Name = "ConsoleAppender",
-                Threshold = Level.Info
-            };
-            csl.AddMapping(new ColoredConsoleAppender.LevelColors
-            {
-                Level = Level.Error,
-                ForeColor = ColoredConsoleAppender.Colors.Red
-            });
-            csl.AddMapping(new ColoredConsoleAppender.LevelColors
-            {
-                Level = Level.Warn,
-                ForeColor = ColoredConsoleAppender.Colors.Yellow
-            });
-            csl.ActivateOptions();
-            hierarchy.Root.AddAppender(csl);
+            var lf = new LoggerFactory().AddSerilog();
+            lf.AddProvider(memLogProvider);
+            var log = lf.CreateLogger("WexbimCreation");
+            log.LogInformation("Creating wexBIM file from IFC model.");
 
-            hierarchy.Root.Level = Level.Info;
-            hierarchy.Configured = true;
+            // set up xBIM logging. It will use your providers.
+            XbimLogging.LoggerFactory = lf;
 
-            return appender;
+            return memLog;
         }
     }
 
-    public class ErrorAppender: MemoryAppender
+    public class MemoryLogProvider : ILoggerProvider
     {
-        public ErrorAppender()
+        private readonly MemoryLog log;
+
+        public MemoryLogProvider(MemoryLog log)
         {
-            Threshold = Level.Warn;
+            this.log = log;
         }
-        public IEnumerable<LogMessage> Errors => GetEvents()
-            .Where(e => e.Level == Level.Error)
-            .Select(e => new LogMessage { Message = e.RenderedMessage, Exception = e.ExceptionObject });
 
-        public IEnumerable<LogMessage> Warnings => GetEvents()
-            .Where(e => e.Level == Level.Warn)
-            .Select(e => new LogMessage { Message = e.RenderedMessage, Exception = e.ExceptionObject });
+        public Microsoft.Extensions.Logging.ILogger CreateLogger(string categoryName)
+        {
+            return log;
+        }
 
+        public void Dispose()
+        {
+            
+        }
+    }
+
+    public class MemoryLog: Microsoft.Extensions.Logging.ILogger
+    {
+        public IEnumerable<LogMessage> Errors => _events
+            .Where(e => e.Level == LogLevel.Error)
+            .Select(e => new LogMessage { Message = e.Message, Exception = e.Exception });
+
+        public IEnumerable<LogMessage> Warnings => _events
+            .Where(e => e.Level == LogLevel.Warning)
+            .Select(e => new LogMessage { Message = e.Message, Exception = e.Exception });
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return new Scope();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel >= LogLevel.Warning;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _events.Add(new LogEvent { Level = logLevel, Message = formatter(state, exception), Exception = exception });
+        }
+
+        private List<LogEvent> _events = new List<LogEvent>();
+
+        private class LogEvent
+        {
+            public LogLevel Level;
+            public string Message;
+            public Exception Exception;
+        }
+
+        private class Scope : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
     }
 
     public struct LogMessage
