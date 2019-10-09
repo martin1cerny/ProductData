6c1a2137e31418c3ea51bec243d32450114ddc14
Klaus Aengenvoort <klaus.aengenvoort@etask.de>
2019-10-09 18:46:31 +0200

    Add validation of ifcxml files against the xsd schemes #1

--- ValidateSampleFiles.ps1
+++ ValidateSampleFiles.ps1
@@ -1,5 +1,5 @@
 ﻿$BaseFolder = "C:\projects\productdata"
-$BaseFolder = "C:\Development\GitHub\buildingSMART\ProductData"
+#$BaseFolder = "C:\Development\GitHub\buildingSMART\ProductData"
 $ValidatorExe="$BaseFolder\Tools\SchemaValidator\bin\Release\net48\SchemaValidator.exe"
 $SampleFiles = Get-ChildItem -Path "$BaseFolder\SampleFiles"  -Recurse -Include *.ifc,*.ifcXML
 $ExitCode=0
