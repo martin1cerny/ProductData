f561f754805fb69b9ba2708d95b9c593f16e5d98
Klaus Aengenvoort <klaus.aengenvoort@etask.de>
2019-10-09 18:41:34 +0200

    Add validation of ifcxml files against the xsd schemes #1

--- SampleFiles/EULUMDAT_PDT_RH180508/Readme.md
+++
@@ -1,3 +0,0 @@
-Add Excel PDT (WG4 TG2) and Eulumdat file.
-ifcXML PDT section: Table structure of last 2 properties open.
-Next step: filling product data in template
--- ValidateSampleFiles.ps1
+++ ValidateSampleFiles.ps1
@@ -1,22 +1,28 @@
 ﻿$BaseFolder = "C:\projects\productdata"
+$BaseFolder = "C:\Development\GitHub\buildingSMART\ProductData"
 $ValidatorExe="$BaseFolder\Tools\SchemaValidator\bin\Release\net48\SchemaValidator.exe"
 $SampleFiles = Get-ChildItem -Path "$BaseFolder\SampleFiles"  -Recurse -Include *.ifc,*.ifcXML
 $ExitCode=0
 foreach ($file in $SampleFiles | Sort-Object -Property FullName) 
 {
-   Write-Host "++++++++++++++++++++++++++++++++++++++"
-   $filename = $file.FullName.Replace($BaseFolder,"")
-   Write-Host Validating $filename
-   & $ValidatorExe $file.FullName
-   if ($LastExitCode -eq '0') 
-   { 
-     Write-Host "OK: $filename is valid" -ForegroundColor Green
-   }
-   else 
+   if     ((!$file.FullName.Contains("SampleFiles\HC (Italy)\Bricks Catalogue")) `
+      -And (!$file.FullName.Contains("\SampleFiles\NisbetSample")) `
+      -And (!$file.FullName.Contains("\SampleFiles\TriluxLightingProducts\SourceDataFromPimSystem\IFCModelFile")))   
    {
-     Write-Host "ERROR: $filename is not valid" -ForegroundColor Red
-     Write-Host "Please check the validation errors and correct them. Thanks for your contribution!" -ForegroundColor Red
-     $ExitCode=1
+       Write-Host "++++++++++++++++++++++++++++++++++++++"
+       $filename = $file.FullName.Replace($BaseFolder,"")
+       Write-Host Validating $filename
+       & $ValidatorExe $file.FullName
+       if ($LastExitCode -eq '0') 
+       { 
+         Write-Host "OK: $filename is valid" -ForegroundColor Green
+       }
+       else 
+       {
+         Write-Host "ERROR: $filename is not valid" -ForegroundColor Red
+         Write-Host "Please check the validation errors and correct them. Thanks for your contribution!" -ForegroundColor Red
+         $ExitCode=1
+       }
    }
 }
 Exit $ExitCode
